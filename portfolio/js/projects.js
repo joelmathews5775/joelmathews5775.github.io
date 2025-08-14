25d5ddcea85c5dc7e92d8d03cfd8edc6c39790c5
joelmathews5775 <74131953+joelmathews5775@users.noreply.github.com>
2025-08-14 11:49:01 +0530

    JM - Portfolio Website - Final
    This is the final version of the completed site

---
+++ portfolio/js/projects.js
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    thumbnailImgPath: "img/portfolio_preview.svg",
+    altText: "Portfolio website preview image",
+    name: "JM - Portfolio Website",
+    description: "A responsive portfolio website built with HTML, CSS & JavaScript, showcasing my journey and projects as a fronend developer.",
+    previewLink: "index.html",
+    sourceLink: "res/portfolio.zip"
+  }
+];
