cedffd4addc3b7c8c2862b824579e320a9f2aa38
joelmathews5775 <74131953+joelmathews5775@users.noreply.github.com>
2025-08-14 12:53:07 +0530

    Update projects.js
    Minor updates

--- capstoneproject/js/projects.js
+++ capstoneproject/js/projects.js
@@ -1,10 +1,11 @@
 export const projects = [
   {
     thumbnailImgPath: "img/portfolio_preview.svg",
-    altText: "Portfolio website preview image",
+    altText: "Website preview",
     name: "JM - Portfolio Website",
     description: "A responsive portfolio website built with HTML, CSS & JavaScript, showcasing my journey and projects as a fronend developer.",
     previewLink: "index.html",
     sourceLink: "res/portfolio.zip"
   }
+
 ];
